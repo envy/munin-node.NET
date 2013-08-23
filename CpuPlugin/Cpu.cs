cfa5cf8e135a3eeffb5b1daa41a4dfe5b2b660b7
Nico Weichbrodt <nico@weichbrodt.me>
2013-08-23 16:21:24 +0200

    Added interrupt and dpc counter to cpu plugin

--- CpuPlugin/Cpu.cs
+++ CpuPlugin/Cpu.cs
@@ -6,22 +6,30 @@ namespace CpuPlugin
 {
     public class Cpu : PluginBase
     {
-		private PerformanceCounter idleTime, userTime, privilegedTime;
+		private PerformanceCounter _idleTime;
+	    private PerformanceCounter _userTime;
+	    private PerformanceCounter _privilegedTime;
+	    private PerformanceCounter _interruptTime;
+	    private PerformanceCounter _dpcTime;
 
 	    public override void Initialize()
 	    {
-			idleTime = new PerformanceCounter("Processor", "% Idle Time", "_Total");
-			userTime = new PerformanceCounter("Processor", "% User Time", "_Total");
-			privilegedTime = new PerformanceCounter("Processor", "% Privileged Time", "_Total");
-		    idleTime.NextValue();
-		    userTime.NextValue();
-		    privilegedTime.NextValue();
+			_idleTime = new PerformanceCounter("Processor", "% Idle Time", "_Total");
+			_userTime = new PerformanceCounter("Processor", "% User Time", "_Total");
+			_privilegedTime = new PerformanceCounter("Processor", "% Privileged Time", "_Total");
+			_interruptTime = new PerformanceCounter("Processor", "% Interrupt Time", "_Total");
+			_dpcTime = new PerformanceCounter("Processor", "% DPC Time", "_Total");
+		    _idleTime.NextValue();
+		    _userTime.NextValue();
+		    _privilegedTime.NextValue();
+		    _interruptTime.NextValue();
+		    _dpcTime.NextValue();
 	    }
 
 	    public override string GetConfig()
 	    {
 		    return "graph_title CPU usage\n" +
-		           "graph_order user privileged idle\n" +
+		           "graph_order user privileged interrupt dpc idle\n" +
 		           "graph args --base 1000 -r -l 0 --upper-limit 100\n" +
 		           "graph_vlabel %\n" +
 		           "graph_scale no\n" +
@@ -36,6 +44,14 @@ namespace CpuPlugin
 		           "privileged.min 0\n" +
 		           "privileged.draw STACK\n" +
 		           "privileged.info CPU time spent in privileged mode\n" +
+				   "interrupt.label interrupt\n" +
+				   "interrupt.min 0\n" +
+				   "interrupt.draw STACK\n" +
+				   "interrupt.info CPU time spent dealing with interrupts\n" +
+				   "dpc.label dpc\n" +
+				   "dpc.min 0\n" +
+				   "dpc.draw STACK\n" +
+				   "dpc.info CPU time spent in deferred procedure calls (dpc)\n" +
 		           "idle.label idle\n" +
 		           "idle.min 0\n" +
 		           "idle.draw STACK\n" +
@@ -44,8 +60,8 @@ namespace CpuPlugin
 
 	    public override string GetValues()
 	    {
-		    return String.Format("user.value {0}\nprivileged.value {1}\nidle.value {2}", DoubleToString(userTime.NextValue()),
-		                         DoubleToString(privilegedTime.NextValue()), DoubleToString(idleTime.NextValue()));
+		    return String.Format("user.value {0}\nprivileged.value {1}\ninterrupt.value {2}\ndpc.value {3}\nidle.value {4}", DoubleToString(_userTime.NextValue()),
+								 DoubleToString(_privilegedTime.NextValue()), DoubleToString(_interruptTime.NextValue()), DoubleToString(_dpcTime.NextValue()), DoubleToString(_idleTime.NextValue()));
 	    }
 
 	    public override string GetName()
